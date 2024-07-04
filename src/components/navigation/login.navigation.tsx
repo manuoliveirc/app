3be94bedcea95c04b50e9aefdaaecb6bb4be10b2
manuoliveirc <manuoliveiravga@gmail.com>
2024-07-04 08:38:23 -0300

    first commit

---
+++ src/components/navigation/login.navigation.tsx
@@ -0,0 +1,22 @@
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
+import { ScreenLogin, ScreenRegister } from '../screens';
+import { AntDesign } from '@expo/vector-icons';
+
+type LoginStackParamList = {
+    Login: undefined
+    Register: undefined
+}
+type LoginScreenNavigationProp = StackNavigationProp<LoginStackParamList, 'Login'>
+export type LoginTypes = {
+    navigation: LoginScreenNavigationProp
+}
+
+export function LoginNavigation() {
+    const Stack = createStackNavigator<LoginStackParamList>();
+    return (
+        <Stack.Navigator id='login' screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='Login' component={ScreenLogin} />
+            <Stack.Screen name='Register' component={ScreenRegister} />
+        </Stack.Navigator>
+    )
+}
---
+++ src/components/screens/index.tsx
@@ -0,0 +1,2 @@
+export {Page1 as ScreenPage1} from './Login'
+export {Page2 as ScreenPage2} from './Register'
